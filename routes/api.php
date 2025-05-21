d4561f4bb146aeb2baf6e88871d607724253aa8e
DESKTOP-6QFCR0F\user <jasonlipreso@gmail.com>
2025-05-22 07:47:13 +0800

    Updates

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'api'], function () {
     Route::group(['prefix' => 'notification'], function () {
         Route::get('test', function () {
-            echo "It worked!";
+            return [];
         });
     });
 });
